0a292cc42e6468db4d2e6326128347a82821cbdc
gragnad <gragnad88@gmail.com>
2022-10-09 23:54:29 +0900

    first commit

---
+++ src/main/java/nalstudio/controller/ProfileController.java
@@ -0,0 +1,30 @@
+package nalstudio.controller;
+
+import core.db.DataBase;
+import nalstudio.model.User;
+import nalstudio.util.UserSessionUtils;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/users/profile")
+public class ProfileController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            throw new NullPointerException("사용자를 찾을 수 없습니다.");
+        }
+        req.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
+        RequestDispatcher rd = req.getRequestDispatcher("/user/profile.jsp");
+        rd.forward(req, resp);
+    }
+}
